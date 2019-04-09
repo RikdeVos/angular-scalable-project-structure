0528c25c9373539e8a47deef6d0e877846a54877
Rik de Vos <rik@rikdevos.com>
2019-04-09 19:42:48 +0200

    inject core providers

--- src/app/feature-a/components/feature-a/feature-a.component.ts
+++ src/app/feature-a/components/feature-a/feature-a.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LocalStorageService, AuthService } from '../../../core/services';
+
 @Component({
   selector: 'app-feature-a',
   templateUrl: './feature-a.component.html',
   styleUrls: ['./feature-a.component.sass']
 })
 export class FeatureAComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private localStorageService: LocalStorageService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {}
 }
